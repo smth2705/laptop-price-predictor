c91483d7c7309d906b130da158caf241d05c228e
smth2705 <samarthsinghal321@gmail.com>
2025-08-15 20:43:05 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+st.title("Laptop Price Predictor")
+company = st.selectbox('Brand', df['Company'].unique())
+type = st.selectbox('Type', df['TypeName'].unique())
+ram = st.selectbox('RAM (in GB)', df['Ram'].unique())
+weight = st.number_input("Weight")
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+ips = st.selectbox('IPS',['No','Yes'])
+screensize = st.number_input("Screensize")
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+hdd = st.selectbox('HDD (in GB)', [0,128,256,512,1024,2048])
+ssd = st.selectbox('SSD (in GB)', [0,128,256,512,1024,2048])
+gpu = st.selectbox('GPU',df['Gpu'].unique())
+os = st.selectbox('OS', df['OpSys'].unique())
+if st.button('Predict Price'):
+    ppi = None
+    if touchscreen=='Yes':
+        touchscreen=1
+    else:
+        touchscreen=0
+    if ips=='Yes':
+        ips=1
+    else:
+        ips=0
+    x_res = int(resolution.split('x')[0])
+    y_res = int(resolution.split('x')[1])
+    ppi = ((x_res**2)+(y_res**2))**0.5/screensize
+    query = np.array([company,type,ram,gpu,os,weight,touchscreen,ips,ppi,cpu,hdd,ssd])
+    query = query.reshape(1,12)
+    st.title("The Predicted price of the configuration is "+ str(int(np.exp(pipe.predict(query)[0]))))
